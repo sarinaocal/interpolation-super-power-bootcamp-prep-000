366a6a5a0ae4fee2d6072e7cca862629609c2b15
Sarina Ocal <sarinaocal@gmail.com>
2020-06-14 01:17:25 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 def display_rainbow(colors) #definre a method that takes in a single arguemnt the arguements name is colors
 
 
-puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}" 
+puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 
 #outs output in the terminla
 
