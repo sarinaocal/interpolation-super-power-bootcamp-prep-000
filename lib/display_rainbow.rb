9ed95cbb2b95519a680de6fbb50ba08faacadae6
Sarina Ocal <sarinaocal@gmail.com>
2020-06-14 01:01:09 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,17 +1,10 @@
 def display_rainbow(colors)
 
-   "R: Red;
-    O: Orange;
-    Y: Yellow;
-    G: Green;
-    B: Blue;
-    I: Indigo;
-    V: Violet"
 
 puts "#{red[0]}; #{orange[1]}; #{yellow[2]}; #{green[3]}; #{blue[4]; #{indigo[5]}; #{violet[6]}
 
-
-
-end
+let colors = ()
 
   display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+
+end
